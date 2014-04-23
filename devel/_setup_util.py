2a99855601513323ccb87720bf5befa9e7098afd
Yongxu Ren <renyongxu@gmail.com>
2014-04-23 05:10:12 -0500

    first commit

--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -252,7 +252,7 @@ if __name__ == '__main__':
             sys.exit(1)
 
         # environment at generation time
-        CMAKE_PREFIX_PATH = '/home/avp/avp/devel;/opt/ros/hydro'.split(';')
+        CMAKE_PREFIX_PATH = '/home/avp/avp_ros/devel;/home/avp/avp/devel;/opt/ros/hydro'.split(';')
         # prepend current workspace if not already part of CPP
         base_path = os.path.dirname(__file__)
         if base_path not in CMAKE_PREFIX_PATH:
